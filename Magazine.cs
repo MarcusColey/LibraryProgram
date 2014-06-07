93f6b8d35af1e71d6deaedf7fdfbfa8b101f0470
Marcus Coley <marcus.09@live.com>
2014-06-07 13:41:42 -0700

    Booktype
    The magazine book type

---
+++ Magazine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryProgram
+{
+    [Serializable]
+    public class Magazine
+    {
+        string name;
+        int issueYear;
+        int issueNumber;
+        int uniqueID;
+
+        public Magazine(string name, int issueYear, int issueNumber, int uniqueID)
+        {
+            this.name = name;
+            this.issueYear = issueYear;
+            this.issueNumber = issueNumber;
+            this.uniqueID = uniqueID;
+        }
+    }
+}
