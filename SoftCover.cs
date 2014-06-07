f998dacc4195bd76be9984a04a8c4ed3bf7ca6b0
Marcus Coley <marcus.09@live.com>
2014-06-07 13:41:55 -0700

    Booktype
    The soft cover book type

---
+++ SoftCover.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryProgram
+{
+    [Serializable]
+    public class SoftCover : Book
+    {
+        public override void CreateBook(string name, string author, CommonFields.Genres genre, CommonFields.BookType coverType, float price, int uniqueID)
+        {
+            bookName = name;
+            bookAuthor = author;
+            bookGenre = genre;
+            bookType = coverType;
+            bookPrice = price;
+            bookUniqueID = uniqueID;
+            date = DateTime.Now;
+        }
+    }
+}
