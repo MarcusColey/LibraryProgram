8b87fab34a245b220bcd96c466be3023605b2532
Marcus Coley <marcus.09@live.com>
2014-06-07 13:41:17 -0700

    enums that are commonly used throughout
    enums that are commonly used throughout

---
+++ CommonFields.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryProgram
+{
+    public static class CommonFields
+    {
+        public enum Genres { Adventure, Horror, Romance, Education }
+        public enum BookType { Soft, Hard }
+    }
+}
